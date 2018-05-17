db9085ef951dcef5ef02e3a6b731011e25c1c075
trishlstark <trishlstark@gmail.com>
2018-05-16 20:42:45 -0600

    Added form to create chat rooms

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -4,7 +4,10 @@ export class RoomList extends Component {
   constructor(props){
     super(props);
     this.roomsRef = this.props.firebase.database().ref('rooms');
+    this.createRoom = this.createRoom.bind(this);
+    this.handleChange = this.handleChange.bind(this);
     this.state = {
+      roomName:"",
       rooms: []
     };
   }
@@ -14,21 +17,40 @@ export class RoomList extends Component {
       const room = snapshot.val();
       room.key = snapshot.key;
       console.log(snapshot);
-      this.setState({ rooms: this.state.rooms.concat( room ) })
+      this.setState({ rooms: this.state.rooms.concat( room ) });
     });
   }
 
+  createRoom(e){
+    e.preventDefault();
+    this.roomsRef.push({roomName: this.state.roomName});
+    this.setState({roomName: ""});
+  }
+
+  handleChange(e){
+    this.setState({roomName: e.target.value});
+  }
 
   render() {
 
+      const roomForm = (
+        <form onSubmit={this.createRoom}> 
+          <input type="text" value={this.state.roomName} onChange={this.handleChange}/>
+          <input type="submit" value="Add Room"/>
+        </form>
+      )
+
       const listOfRooms = this.state.rooms.map((room) =>
         <li key={room.key}>
-          {room.name}
+          {room.roomName}
         </li>
       );
 
       return(
-        <ul>{listOfRooms}</ul>
+        <div>
+          <ul>{listOfRooms}</ul>
+          <div>{roomForm}</div>
+        </div>
       );
     }
 
